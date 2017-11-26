bdf875d77ccb0fb0ee80e146ef8ee092c20421e3
OSPFNeighbour <ttdykes@gmail.com>
2017-11-26 20:53:45 +1100

    cleanup

---
+++ arduino/V6_219_RAW/V6_Buffered/V6_Buffered.ino
@@ -0,0 +1,227 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_INA219.h>
+
+Adafruit_INA219 ina219_battery;
+Adafruit_INA219 ina219_solar(0x41);
+
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+// Buffer sizes
+#define NUM_SAMPLES 200
+
+float LowBattShutdown = 11.0; //shutdown voltage
+float LowBattStartup = 11.5; //shutdown voltage
+
+
+
+int Holddown = 20; //prevent flapping of the system, wait time between exec commands
+
+
+//Pins used
+int Pirelay = 2;
+int PiRunningPort = 4;
+int PiSignalShutdownPort = 3;
+
+unsigned long LastPowerActionMilli = -10000;
+
+int ProcessState = 1;
+//0 = Running normal, pi on
+//1 = Running normal, pi off
+//2 = Pi is off due to power
+//3 = Pi is shutting down
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Booting...");
+  pinMode(PiRunningPort, INPUT);
+  pinMode(PiSignalShutdownPort, OUTPUT);
+  digitalWrite(PiSignalShutdownPort, LOW);
+  // Initialize the INA219.
+  // By default the initialization will use the largest range (32V, 2A).  However
+  // you can call a setCalibration function to change this range (see comments).
+  ina219_battery.begin();
+  ina219_solar.begin();
+
+  // To use a slightly lower 32V, 1A range (higher precision on amps):
+  //ina219.setCalibration_32V_1A();
+  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+  //ina219.setCalibration_16V_400mA();
+
+  LastPowerActionMilli = 0;
+  pinMode(Pirelay, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+
+  float battery_shuntvoltage = 0;
+  float battery_busvoltage = 0;
+  float battery_current_mA = 0;
+  float battery_loadvoltage = 0;
+  float Vccvoltage = readVcc() / 1000;
+
+  battery_shuntvoltage = ina219_battery.getShuntVoltage_mV();
+  battery_busvoltage = ina219_battery.getBusVoltage_V();
+  battery_current_mA = ina219_battery.getCurrent_mA();
+  battery_loadvoltage = battery_busvoltage + (battery_shuntvoltage / 1000);
+
+  
+  lcd.setCursor(0, 1);
+  lcd.print("B:");
+  lcd.print(battery_loadvoltage);
+  lcd.print("V S:");
+  lcd.print("0");
+  lcd.print("V");
+
+  Serial.print("|");
+  Serial.print("S:");
+  Serial.print("0");
+  Serial.print("|");
+  Serial.print("B:");
+  Serial.print(battery_loadvoltage);
+  Serial.print("|");
+  Serial.print("R:");
+  Serial.print(Vccvoltage);
+  Serial.print("|");
+  Serial.print("W:");
+  Serial.print(battery_current_mA);
+  Serial.print("|");
+  Serial.println("");
+  unsigned long mils = millis();
+  if (mils - LastPowerActionMilli >= (Holddown * 1000))
+  {
+    switch (ProcessState) {
+      //0 = Running normal, pi on
+      //1 = Running normal, pi off
+      //2 = Pi is off due to power low
+      //3 = Pi is shutting down
+      case 0:
+        lcd.setCursor(0, 0);
+        if (digitalRead(PiRunningPort) == HIGH)
+        {
+          lcd.print(" PI IS RUNNING! ");
+        }
+        if (battery_loadvoltage < LowBattShutdown) //voltage is LOW
+        {
+          lcd.setCursor(0, 0);
+          lcd.print("SHUTTING DOWN PI");
+          // //Serial.println("Shutting down the Pi");
+          digitalWrite(PiSignalShutdownPort, HIGH);    // Signal the pi to shutdown
+          ProcessState = 3;
+          LastPowerActionMilli = millis();
+        }
+        break;
+      case 1:
+        lcd.setCursor(0, 0);
+        lcd.print(" PI POWERED OFF ");
+        if (battery_loadvoltage >= LowBattStartup) //voltage is OK
+        {
+          lcd.setCursor(0, 0);
+          lcd.print("    RUNNING     ");
+          ////Serial.println("Starting up the Pi");
+          digitalWrite(Pirelay, HIGH);    // Start the PI
+          digitalWrite(PiSignalShutdownPort, LOW);
+          ProcessState = 0;
+          LastPowerActionMilli = millis();
+        }
+        break;
+      case 2:
+        lcd.setCursor(0, 0);
+        lcd.print(" PWR LOW PI OFF! ");
+        if (LowBattStartup >= LowBattShutdown) //voltage is OK again
+        {
+          lcd.setCursor(0, 0);
+          lcd.print("  PI START UP!  ");
+          //Serial.println("Starting up the Pi");
+          digitalWrite(Pirelay, HIGH);    // Start the PI
+          digitalWrite(PiSignalShutdownPort, LOW);
+          ProcessState = 0;
+          LastPowerActionMilli = millis();
+        }
+        break;
+      case 3:
+        lcd.setCursor(0, 0);
+        lcd.print("PI SHUTTING DOWN");
+        if (digitalRead(PiRunningPort) == LOW) //if the pi has shutdown
+        {
+          lcd.setCursor(0, 0);
+          lcd.print("BATT LOW PI OFF");
+          //Serial.println("Killing power to the Pi");
+          digitalWrite(Pirelay, LOW);    // Kill power to the PI
+          ProcessState = 2;
+          LastPowerActionMilli = millis();
+
+        }
+        break;
+    }
+  } else {
+    //Serial.println("waiting for holdown to expire");
+  }
+
+  delay(1000);
+}
+
+
+float readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA, ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
+  uint8_t high = ADCH; // unlocks both  sw
+
+  long result = (high << 8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
+float readValue(int sensorpin) {
+  // read multiple values and sort them to take the mode
+  int sortedValues[NUM_SAMPLES];
+  for (int i = 0; i < NUM_SAMPLES; i++) {
+    int value = analogRead(sensorpin);
+    int j;
+    if (value < sortedValues[0] || i == 0) {
+      j = 0; //insert at first position
+    }
+    else {
+      for (j = 1; j < i; j++) {
+        if (sortedValues[j - 1] <= value && sortedValues[j] >= value) {
+          // j is insert position
+          break;
+        }
+      }
+    }
+    for (int k = i; k > j; k--) {
+      // move all values higher than current reading up one position
+      sortedValues[k] = sortedValues[k - 1];
+    }
+    sortedValues[j] = value; //insert current reading
+  }
+  //return scaled mode of 10 values
+  float returnval = 0;
+  for (int i = NUM_SAMPLES / 2 - 5; i < (NUM_SAMPLES / 2 + 5); i++) {
+    returnval += sortedValues[i];
+  }
+  returnval = returnval / 10;
+  return returnval;
+}
---
+++ arduino/V6_219_RAW/V6_Sleep/V6_Sleep/V6_Sleep.ino
@@ -0,0 +1,294 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_INA219.h>
+#include "LowPower.h"
+
+Adafruit_INA219 ina219_battery;
+Adafruit_INA219 ina219_solar(0x41);
+
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+float LowBattShutdown = 11.35; //shutdown voltage
+float LowBattStartup = 11.5; //shutdown voltage
+
+
+
+int Holddown = 2; //2*8 = 16 sec - prevent flapping of the system, wait X cycles between exec commands
+const int AverageOverThisMayLoops = 5; //5*8 = 40 secs
+
+//Pins used
+int Pirelay = 2;
+int PiRunningPort = 4;
+int PiSignalShutdownPort = 3;
+int RunningLED = 9;
+
+unsigned long LastAction = 0;
+
+int ProcessState = 1;
+//0 = Running normal, pi on
+//1 = Running normal, pi off
+//2 = Pi is off due to power
+//3 = Pi is shutting down
+
+float avg_battery_shuntvoltage[AverageOverThisMayLoops];
+float avg_battery_busvoltage[AverageOverThisMayLoops];
+float avg_battery_current_mA[AverageOverThisMayLoops];
+float avg_battery_loadvoltage[AverageOverThisMayLoops];
+
+float avg_solar_shuntvoltage[AverageOverThisMayLoops];
+float avg_solar_busvoltage[AverageOverThisMayLoops];
+float avg_solar_current_mA[AverageOverThisMayLoops];
+float avg_solar_loadvoltage[AverageOverThisMayLoops];
+
+float avg_Vccvoltage[AverageOverThisMayLoops];
+
+
+
+int AvgPosition = 0;
+int LoopCounter = 0;
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Booting...");
+  pinMode(PiRunningPort, INPUT);
+  pinMode(PiSignalShutdownPort, OUTPUT);
+  digitalWrite(PiSignalShutdownPort, LOW);
+  // Initialize the INA219.
+  // By default the initialization will use the largest range (32V, 2A).  However
+  // you can call a setCalibration function to change this range (see comments).
+  ina219_battery.begin();
+  ina219_solar.begin();
+
+  ina219_battery.setCalibration_32V_1A();
+  ina219_solar.setCalibration_32V_1A();
+
+  // To use a slightly lower 32V, 1A range (higher precision on amps):
+  //ina219.setCalibration_32V_1A();
+  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+  //ina219.setCalibration_16V_400mA();
+
+  pinMode(Pirelay, OUTPUT);
+    pinMode(RunningLED, OUTPUT);
+
+  Serial.begin(9600);
+
+  //Avg buffer warmup
+  for (int i = 0; i <= AverageOverThisMayLoops; i++) {
+    avg_battery_shuntvoltage[i] = ina219_battery.getShuntVoltage_mV();
+    avg_battery_busvoltage[i] = ina219_battery.getBusVoltage_V();
+    avg_battery_current_mA[i] = ina219_battery.getCurrent_mA();
+    avg_battery_loadvoltage[i] = avg_battery_busvoltage[i] + (avg_battery_shuntvoltage[i] / 1000);
+    
+    avg_solar_shuntvoltage[i] = ina219_solar.getShuntVoltage_mV();
+    avg_solar_busvoltage[i] = ina219_solar.getBusVoltage_V();
+    avg_solar_current_mA[i] = ina219_solar.getCurrent_mA();
+    avg_solar_loadvoltage[i] = avg_solar_busvoltage[i] + (avg_solar_shuntvoltage[i] / 1000);
+    
+    avg_Vccvoltage[i] = readVcc() / 1000;
+
+  }
+  lcd.setCursor(0, 0);
+  lcd.print("....Running!...");
+  digitalWrite(RunningLED, HIGH);
+
+}
+
+
+void loop()
+{
+
+  LowPower.idle(SLEEP_4S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
+                SPI_OFF, USART0_OFF, TWI_OFF);
+
+  //Sample every 4 sec, print every 8
+
+  float battery_shuntvoltage = 0;
+  float battery_busvoltage = 0;
+  float battery_current_mA = 0;
+  float battery_loadvoltage = 0;
+  
+  float solar_shuntvoltage = 0;
+  float solar_busvoltage = 0;
+  float solar_current_mA = 0;
+  float solar_loadvoltage = 0;
+  
+  float Vccvoltage = readVcc() / 1000;
+
+  battery_shuntvoltage = ina219_battery.getShuntVoltage_mV();
+  battery_busvoltage = ina219_battery.getBusVoltage_V();
+  battery_current_mA = ina219_battery.getCurrent_mA();
+  battery_loadvoltage = battery_busvoltage + (battery_shuntvoltage / 1000);
+  
+  
+  solar_shuntvoltage = ina219_solar.getShuntVoltage_mV();
+  solar_busvoltage = ina219_solar.getBusVoltage_V();
+  solar_current_mA = ina219_solar.getCurrent_mA();
+  solar_loadvoltage = solar_busvoltage + (solar_shuntvoltage / 1000);
+
+  avg_battery_shuntvoltage[AvgPosition] = battery_shuntvoltage;
+  avg_battery_busvoltage[AvgPosition] = battery_busvoltage;
+  avg_battery_current_mA[AvgPosition] = battery_current_mA;
+  avg_battery_loadvoltage[AvgPosition] = battery_loadvoltage;
+
+  avg_solar_shuntvoltage[AvgPosition] = solar_shuntvoltage;
+  avg_solar_busvoltage[AvgPosition] = solar_busvoltage;
+  avg_solar_current_mA[AvgPosition] = solar_current_mA;
+  avg_solar_loadvoltage[AvgPosition] = solar_loadvoltage;
+  
+  avg_Vccvoltage[AvgPosition] = readVcc() / 1000;
+
+  if (AvgPosition == AverageOverThisMayLoops - 1) {
+    AvgPosition = 0;
+  } else {
+    AvgPosition = AvgPosition + 1;
+  }
+
+  if (LoopCounter == 1)
+  {
+    LoopCounter = 0;
+    //  float avgtotal_battery_shuntvoltage = 0;
+    //  float avgtotal_battery_busvoltage = 0;
+    //  float avgtotal_battery_current_mA = 0;
+    //  float avgtotal_battery_loadvoltage = 0;
+    //  float avgtotal_Vccvoltage = 0;
+    //  int AvgPosition = 0;
+
+    lcd.setCursor(0, 1);
+    lcd.print("BV:");
+    lcd.print(battery_loadvoltage);
+    lcd.print(" SV:");
+    lcd.print(solar_loadvoltage);
+    
+
+    Serial.print("|");
+    Serial.print("S:");
+    Serial.print(average(avg_solar_loadvoltage, AverageOverThisMayLoops));
+    Serial.print("|");
+    Serial.print("B:");
+    Serial.print(average(avg_battery_loadvoltage, AverageOverThisMayLoops));
+    Serial.print("|");
+    Serial.print("R:");
+    Serial.print(average(avg_Vccvoltage, AverageOverThisMayLoops));
+    Serial.print("|");
+    Serial.print("W:");
+    Serial.print(average(avg_battery_current_mA, AverageOverThisMayLoops) * average(avg_battery_loadvoltage, AverageOverThisMayLoops));
+    Serial.print("|");
+    Serial.print("M:");
+    Serial.print(average(avg_solar_current_mA, AverageOverThisMayLoops) * average(avg_solar_loadvoltage, AverageOverThisMayLoops));
+    Serial.println("|");
+
+    if (LastAction >= Holddown)
+    {
+      LastAction = 0;
+      switch (ProcessState) {
+        //0 = Running normal, pi on
+        //1 = Running normal, pi off
+        //2 = Pi is off due to power low
+        //3 = Pi is shutting down
+        case 0:
+          lcd.setCursor(0, 0);
+          if (digitalRead(PiRunningPort) == HIGH)
+          {
+            lcd.print(" PI IS RUNNING! ");
+          } else {
+            lcd.print("PI IS NO RUNING!");
+          }
+          if (average(avg_battery_loadvoltage, AverageOverThisMayLoops) < LowBattShutdown) //voltage is LOW
+          {
+            lcd.setCursor(0, 0);
+            lcd.print("SHUTTING DOWN PI");
+            // //Serial.println("Shutting down the Pi");
+            digitalWrite(PiSignalShutdownPort, HIGH);    // Signal the pi to shutdown
+            ProcessState = 3;
+          }
+          break;
+        case 1:
+          lcd.setCursor(0, 0);
+          lcd.print(" PI POWERED OFF ");
+          if (average(avg_battery_loadvoltage, AverageOverThisMayLoops) >= LowBattStartup) //voltage is OK
+          {
+            lcd.setCursor(0, 0);
+            lcd.print("    RUNNING     ");
+            ////Serial.println("Starting up the Pi");
+            digitalWrite(Pirelay, HIGH);    // Start the PI
+            ProcessState = 0;
+          }
+          break;
+        case 2:
+          lcd.setCursor(0, 0);
+          lcd.print(" PWR LOW PI OFF! ");
+          if (average(avg_battery_loadvoltage, AverageOverThisMayLoops) >= LowBattStartup) //voltage is OK again
+          {
+            lcd.setCursor(0, 0);
+            lcd.print("  PI START UP!  ");
+            //Serial.println("Starting up the Pi");
+            digitalWrite(Pirelay, HIGH);    // Start the PI
+            ProcessState = 0;
+          }
+          break;
+        case 3:
+          lcd.setCursor(0, 0);
+          lcd.print("PI SHUTTING DOWN");
+          if (digitalRead(PiRunningPort) == LOW) //if the pi has shutdown
+          {
+            lcd.setCursor(0, 0);
+            lcd.print("BATT LOW PI OFF");
+            //Serial.println("Killing power to the Pi");
+            digitalWrite(Pirelay, LOW);    // Kill power to the PI
+            digitalWrite(PiSignalShutdownPort, LOW); //Low the shutdown port
+            ProcessState = 2;
+          }
+          break;
+      }
+    } else {
+      LastAction = LastAction + 1;
+    }
+  } else {
+    LoopCounter++;
+  }
+
+  //sleepy time
+ Serial.flush();
+
+}
+
+
+float readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA, ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
+  uint8_t high = ADCH; // unlocks both  sw
+
+  long result = (high << 8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
+float average (float * array, int len)  // assuming array is int.
+{
+  float sum = 0 ;  // sum will be larger than an item, long for safety.
+  for (int i = 0 ; i < len ; i++)
+  {
+    sum =  (float)sum + (float)array [i] ;
+  }
+
+  return  ((float) sum) / len ;  // average will be fractional, so float may be appropriate.
+}
